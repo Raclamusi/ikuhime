de80595e40ae0455d0210065639a534f4a2ee0de
Raclamusi <rintarou.tohgo.1203@gmail.com>
2020-10-18 22:51:13 +0900

    Add index.html etc.

---
+++ docs/script/top.js
@@ -0,0 +1,33 @@
+
+var canvas = document.getElementById("main-canvas");
+var ctx = canvas.getContext("2d");
+
+var level = parseInt(localStorage.getItem("level")) || 1;
+var experience = parseInt(localStorage.getItem("experience")) || 0;
+
+var avatar = document.getElementById("avatar");
+
+function Resize() {
+    canvas.width = window.innerWidth;
+}
+
+function Draw() {
+    ctx.font = "32px serif";
+    ctx.fillText("Lv. " + level, 30, 62);
+    ctx.fillStyle = "#33ff66";
+    ctx.fillRect(30, 70, 20 * experience, 20);
+    ctx.strokeStyle = "#333399";
+    ctx.lineWidth = 5;
+    ctx.strokeRect(30, 70, 200, 20);
+    ctx.drawImage(avatar, 0, 0, avatar.clientWidth, avatar.clientHeight, (canvas.width - 256) / 2, 100, 256, 256);
+}
+
+window.onload = function () {
+    Resize();
+    Draw();
+};
+
+window.onresize = function () {
+    Resize();
+    Draw();
+};
