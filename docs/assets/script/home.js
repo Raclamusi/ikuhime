40b40d344bedf8fe3071b93f53c8f62783f96017
raclamusi <raclamusi@gmail.com>
2021-01-28 23:24:01 +0900

    Integrated the messages of growing.

--- docs/assets/script/home.js
+++ docs/assets/script/home.js
@@ -4,25 +4,34 @@ window.addEventListener("load", function () {
     var levelSpan = document.getElementById("avatar-level");
     var expSpan = document.getElementById("avatar-exp")
     var expBar = document.getElementById("avatar-expbar");
+    var preLevel = userdata.level;
     while (userdata.exp >= userdata.level * 100 + 400) {
         userdata.exp -= userdata.level * 100 + 400;
         userdata.level++;
-        setTimeout(window.alert, 500, "レベルアップしました！\nLv. " + (userdata.level - 1) + " → " + userdata.level);
     }
+    if (userdata.level > preLevel) {
+        setTimeout(window.alert, 500, "レベルアップしました！\nLv. " + preLevel + " → " + userdata.level);
+	}
     userdata.save("exp", "level");
     nameSpan.textContent = userdata.name;
     levelSpan.textContent = userdata.level + "";
     expSpan.textContent = userdata.level * 100 + 400 - userdata.exp + "";
     expBar.style.width = userdata.exp * 100 / (userdata.level * 100 + 400) + "%";
 
+    var message = "実績解除！\n装備を獲得しました！\n\n";
+    var released = false;
     for (var i = 0; i < achievementList.length; i++) {
         var ac = achievementList[i];
         if (!userdata.enableEquip[ac.equip]) {
             if (ac.condition.check()) {
                 userdata.enableEquip[ac.equip] = true;
-                setTimeout(window.alert, 500, "実績解除！「" + ac.title + "」\n装備を獲得しました！");
+                message += "「" + ac.title + "」\n";
+                released = true;
 			}
 		}
+    }
+    if (released) {
+        setTimeout(window.alert, 500, message);
     }
     userdata.save("enableEquip");
 
