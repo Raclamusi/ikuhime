1f7953591354cf92946da124b0b83339abd64dca
raclamusi <raclamusi@gmail.com>
2021-02-24 02:19:51 +0900

    Fixed a bug on IE!

--- docs/assets/script/polyfill.js
+++ docs/assets/script/polyfill.js
@@ -1,10 +1,26 @@
 
 (function () {
-	const isIE = document.documentMode && document.uniqueID;
-	if (isIE) {
-		const polyfill = document.createElement("script");
-		polyfill.type = "text/javascript";
-		polyfill.src = "https://polyfill.io/v3/polyfill.min.js?features=es2015%2Ces2016%2Ces2017";
-		document.getElementsByTagName("head")[0].appendChild(polyfill);
-	}
+    const isIE = document.documentMode && document.uniqueID;
+    if (isIE) {
+        const polyfill = document.createElement("script");
+        polyfill.type = "text/javascript";
+        polyfill.src = "https://polyfill.io/v3/polyfill.min.js?features=es2015%2Ces2016%2Ces2017";
+        document.getElementsByTagName("head")[0].appendChild(polyfill);
+
+        (function (arr) {
+            arr.forEach(function (item) {
+                if (item.hasOwnProperty('remove')) {
+                    return;
+                }
+                Object.defineProperty(item, 'remove', {
+                    configurable: true,
+                    enumerable: true,
+                    writable: true,
+                    value: function remove() {
+                       this.parentNode.removeChild(this);
+                    }
+                });
+            });
+        })([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+    }
 })();
