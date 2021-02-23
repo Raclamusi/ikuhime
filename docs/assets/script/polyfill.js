3ec73b3c7cd999419a9d2e8484ed000c628ce0d8
raclamusi <raclamusi@gmail.com>
2021-02-24 01:57:02 +0900

    Fixed a bug of "IE!!!!!"
    add loading JavaScript versions in the polyfill

--- docs/assets/script/polyfill.js
+++ docs/assets/script/polyfill.js
@@ -4,7 +4,7 @@
 	if (isIE) {
 		const polyfill = document.createElement("script");
 		polyfill.type = "text/javascript";
-		polyfill.src = "https://polyfill.io/v3/polyfill.min.js";
+		polyfill.src = "https://polyfill.io/v3/polyfill.min.js?features=es2015%2Ces2016%2Ces2017";
 		document.getElementsByTagName("head")[0].appendChild(polyfill);
 	}
 })();
