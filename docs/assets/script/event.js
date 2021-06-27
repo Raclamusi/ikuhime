79bc632bf021ac37f6a7be7917a1bbb581f9d2c8
raclamusi <raclamusi@gmail.com>
2021-06-27 16:34:25 +0900

    Improved the records of event page.

--- docs/assets/script/event.js
+++ docs/assets/script/event.js
@@ -1,22 +1,37 @@
 
 (function () {
-    const list = document.getElementById("list");
-    userdata.eventRecords.forEach(function (record) {
-        const li = document.createElement("li");
-        const section = document.createElement("section");
-        const date = document.createElement("p");
-        const title = document.createElement("h2");
-        const content = document.createElement("p");
-        date.textContent = record.date;
-        date.classList.add("event-date");
-        title.textContent = record.title;
-        title.classList.add("event-title");
-        content.textContent = record.content;
-        content.classList.add("event-content");
-        section.appendChild(date);
-        section.appendChild(title);
-        section.appendChild(content);
-        li.appendChild(section);
-        list.appendChild(li);
-    });
+    const searcher = document.getElementById("search");
+    const updateList = function () {
+        const searchedText = new RegExp(searcher.value, "i");
+        const oldList = document.getElementById("list");
+        const newList = oldList.cloneNode(false);
+        userdata.eventRecords.filter(function (record) {
+            return ["title", "content", "date"].some(function (key) {
+                return record[key].search(searchedText) !== -1;
+            });
+        }).forEach(function (record) {
+            const li = document.createElement("li");
+            const section = document.createElement("section");
+            const date = document.createElement("p");
+            const title = document.createElement("h2");
+            date.textContent = record.date;
+            date.classList.add("event-date");
+            title.textContent = record.title;
+            title.classList.add("event-title");
+            section.appendChild(date);
+            section.appendChild(title);
+            record.content.split("\n").forEach(function (para) {
+                const p = document.createElement("p");
+                p.textContent = para;
+                p.classList.add("event-content");
+                section.appendChild(p);
+            });
+            li.appendChild(section);
+            newList.appendChild(li);
+        });
+        oldList.parentNode.replaceChild(newList, oldList);
+    };
+    searcher.addEventListener("change", updateList);
+    
+    updateList();
 })();
